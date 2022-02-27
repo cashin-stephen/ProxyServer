82a59d7bc6f71c5bdf791b99d38dfe7adf5582b5
Stephen Cashin <cashins@tcd.ie>
2022-02-27 13:11:06 +0000

    added String matching to determine protocol type

--- proxy.java
+++ proxy.java
@@ -1,4 +1,7 @@
 import java.net.*;
+
+import javax.print.attribute.standard.RequestingUserName;
+
 import java.io.*;
 
 
@@ -17,19 +20,30 @@ public class proxy {
     public void start(int port) throws Exception{
         serverSocket = new ServerSocket(port);
         clientSocket = serverSocket.accept();
-        //while (true) {
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            String greeting = in.readLine();
-            System.out.println(greeting);
-            out.println("ACK");
-                // if ("hello server".equals(greeting)) {
-                //     System.out.println("hello client");
-                // }
-                // else {
-                //     out.println("unrecognised greeting");
-                // }
-        //}
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String url = in.readLine();
+        System.out.println(url);
+        out.println("ACK");
+
+        //determine http or https
+        if(url.matches("https.*")) {
+            requestHttps();
+        }
+        else if(url.matches("http.*")) {
+            requestHttp();
+        }
+        else {
+            System.out.println("Bad URL");
+        }
+    }
+
+    public static void requestHttps() {
+        System.out.println("HTTPS Case");
+    }
+
+    public static void requestHttp() {
+        System.out.println("HTTP Case");
     }
 
     public void stop() throws Exception {
